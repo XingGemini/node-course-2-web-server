1c80db9a6af6bd68399afac46e14871ac99446de
Xing Xu <xing@dashgenomic.com>
2017-09-02 22:55:08 -0700

    add a fake test case, and dependency about testing

---
+++ test/server.test.js
@@ -0,0 +1,12 @@
+const expect = require ('expect');
+const request = require ('supertest');
+
+
+describe ('Get /', () => {
+  it ('this is a fake test', (done) => {
+    var text = 'Test todo text';
+
+    expect(3).toBe(3);
+    done();
+  });
+});
